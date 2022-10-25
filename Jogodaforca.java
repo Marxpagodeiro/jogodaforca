293573b1890e1026aed46df90903491262092aaf
Marxpagodeiro <113648003+Marxpagodeiro@users.noreply.github.com>
2022-10-25 20:08:51 -0300

    Add files via upload

--- Jogodaforca.java
+++ Jogodaforca.java
@@ -1,88 +1,91 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
+package jogodaforca;
 
-package com.mycompany.jogodaforca;
-
-import java.util.Random;
 import java.util.Scanner;
 
 /**
  *
- * @author Bruno
+ * @author 20222tpmi0035
  */
 public class Jogodaforca {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
+        // TODO code application logic here
         Scanner in = new Scanner(System.in);
-        Random random = new Random(); // pegar aleatoriamente  
+        String palavra;
+        char letra;
+        int i=0;
+        System.out.println("qual a palavra? ");
+        palavra = in.next();
+        System.out.println("qual é a letra?");
+        letra = in.next().charAt(i);
         
-        // lista de palavras
-        String[] palavras = {"java","logica","programacao","algoritmo","tecnico","segunda","casa","ifes","informatica"};
-        String palavraSort = palavraS(palavras); 
-        String letrasUsadas = "";
-        char letras;
-        char[] letrasCertas = new char[palavraSort.length()];// controlar o acertos do usuario 
-        for(int i =0; i<letrasCertas.length;i++){
-            letrasCertas[i]=0;
-        }
-        int vidas = 6;
-        boolean ganhou = false;
+        char[] enigma = new char[palavra.length()];
+        enigma = sublinhado(enigma);
+        substituiLetra(palavra,letra,enigma);
         
-        for(int i = 0; i<palavraSort.length();i++){
-                System.out.print(" _ ");
-        }
         
-        while (!ganhou && vidas > 0){
-            
-            System.out.println("\n"
-                    +"Você tem " + vidas + " vidas" 
-                    +"\nletras usadas:" + letrasUsadas
-                    +"\nQual a letra que vc deseja tentar? ");
-            letras = in.next().charAt(0);
-            letrasUsadas += " " + letras;
-            
-            boolean erro = true;
-            for(int i = 0; i<palavraSort.length();i++){
-                if(letras == palavraSort.charAt(i)){
-                    System.out.println("A palavra tem essa letra na posição " + (i+1));
-                    letrasCertas[i]= 1;
-                    erro = false;
-                }
-            }
-            if(erro){
-                vidas--;
-            }
-            
-            System.out.println("\n");
-            ganhou = true;
-            for(int i = 0; i<palavraSort.length();i++){
-                if(letrasCertas[i] == 0){
-                    System.out.print(" _ ");
-                    ganhou = false;
-                }else{
-                    System.out.print(" " + palavraSort.charAt(i) + " ");
-                }
-            }
-            System.out.println("\n");
-        }
-        if(vidas<1){
-            System.out.println("\n Você perdeu, mas sorte da proxima vez.");
-            System.out.println("\n A palavra era: " + palavraSort);
-        }else{
-            System.out.println(" \n Voce ganhou,parabéns seu QI é maior que o de um bolsominiom.");
-        }
-        System.out.println("Obrigado por jogar meu jogo");
+       
+        String resp = new String(enigma);
+        System.out.println(resp);
+        
     }
-        // sorteio de palavras
-    public static String palavraS(String[] palavras){
-        Random random = new Random();
-        int quantPa = palavras.length; // tamanho do vetor
-        int indiceSorte = random.nextInt(quantPa); // posição do vetor para palavra sorteada.
-        String palavraSort = palavras[indiceSorte];// palavra sorteada
-        return palavraSort;
-    }
-}
-   
 
+    public static char[] sublinhado(char[] vet){
+       char[] caracteres = new char[vet.length];
+       for(int i = 0;i<vet.length;i++){
+            caracteres[i]='_';
+        }
+        return caracteres;
+    }
+    public static void substituiLetra(String palavra,char letra,char[]enigma){
+         for(int i = 0;i<palavra.length();i++){
+            
+          if(letra == palavra.charAt(i)){
+                
+                enigma[i] = palavra.charAt(i); 
+            }          
+        }
+        
+    }
+    public static boolean buscarRepositorio(char[] vet,char letra){
+        boolean resp = false;
+        for(int i=0;i<vet.length;i++){
+            if(letra==vet[i]){
+                resp=true;
+                break;
+            }
+        }
+        return resp;
+    }
+    public static char[] inserirLetra(char[]vet,char letra){
+        char[] resp = vet;
+        for(int i=0;i<vet.length;i++){
+            if(vet[i]=='_'){
+                vet[i]=letra;
+                break;
+            }
+        }
+        return resp;
+    }
+    public static String listarRepositorio(char[]vet){
+        String resp ="";
+        for(int i=0;i<vet.length;i++){
+            if(vet[i]!='_'){
+                resp = resp + vet[i] + " ";
+            }else{
+                break;
+            }
+        }
+        return resp;
+        
+    }
+    
+}
