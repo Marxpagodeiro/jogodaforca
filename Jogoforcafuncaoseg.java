ca978121a255e2f261283041e4d3877739e32023
Marxpagodeiro <113648003+Marxpagodeiro@users.noreply.github.com>
2022-10-24 22:00:36 -0300

    Update and rename Jogoforcafuncao.java to Jogoforcafuncaoseg.java

--- Jogoforcafuncaoseg.java
+++ Jogoforcafuncaoseg.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  *
  * @author 20222TPMI0035
  */
-public class Jogoforcafuncao {
+public class Jogoforcafuncaoseg {
 
     /**
      * @param args the command line arguments
